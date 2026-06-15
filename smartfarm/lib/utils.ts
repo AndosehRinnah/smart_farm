dc41e8f7b2498f6bbea14e615fc9672c1db6bca7
rinadoseh <rinadoseh@gmail.com>
2026-06-15 18:41:25 +0100

    Did my own part

--- smartfarm/lib/utils.ts
+++ smartfarm/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatXAF(amount: number) {
+  return new Intl.NumberFormat('fr-CM', {
+    style: 'currency',
+    currency: 'XAF',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  }).format(amount).replace('FCFA', 'FCFA').trim();
+}
+
